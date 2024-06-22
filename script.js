a1ec3fc05939ffd3466c1010a46fd41759ee25bc
priyanshubarai <priyanshubarai45@gmail.com>
2024-06-22 16:28:31 +0530

    first run

--- script.js
+++ script.js
@@ -5,5 +5,25 @@ const adj = {
 }
 
 const shop_name ={
-    
+    1: "Engine",
+    2: "Foods",
+    3: "Garments"
 }
+
+const suffix = {
+    1: "Bros",
+    2: "Limited",
+    3: "Hub"
+}
+let GO = false
+GO = confirm("Generate A Bussiness Name");
+
+let num1 = Math.floor(Math.random()*3)
+let num2 = Math.floor(Math.random()*3)
+let num3 = Math.floor(Math.random()*3)
+
+while(GO=true){
+    alert(adj[num1] + " " + shop_name[num2] + " " + suffix[num3])
+}
+
+alert("program end..")
